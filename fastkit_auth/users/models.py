d2116886dd3b0633a810b23ba3359ae5eccbed63
radomirbrkovic <radomir.brkovic@pmf.edu.rs>
2026-01-22 20:38:31 +0100

    Create users model

---
+++ fastkit_auth/users/models.py
@@ -0,0 +1,17 @@
+from sqlalchemy import String, DateTime
+from sqlalchemy.orm import Mapped, mapped_column
+from fastapi_users.db import SQLAlchemyBaseUserTableUUID
+from fastkit_core.database import BaseWithTimestamps
+from typing import Optional
+from datetime import datetime
+
+class User(BaseWithTimestamps, SQLAlchemyBaseUserTableUUID):
+    __tablename__ = "users"
+
+    first_name: Mapped[str | None] = mapped_column(String(255))
+    last_name: Mapped[str | None] = mapped_column(String(255))
+    email_verified_at: Mapped[Optional[datetime]] = mapped_column(
+        DateTime(timezone=True),
+        nullable=True,
+        default=None,
+    )
