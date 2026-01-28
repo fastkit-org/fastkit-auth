5f5b4fb7d5e627fddbee4076bce3c40f82868198
radomirbrkovic <radomir.brkovic@pmf.edu.rs>
2026-01-28 08:35:20 +0100

    Fix user service arguments

--- src/fastkit_auth/users/service.py
+++ src/fastkit_auth/users/service.py
@@ -5,7 +5,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from fastkit_auth.users.models import User
 from fastkit_auth.users.schemas import UserUpdate, UserCreate, UserResponse
 
-class UserService(AsyncBaseCrudService[UserCreate, UserUpdate, UserResponse]):
+class UserService(AsyncBaseCrudService[User, UserCreate, UserUpdate, UserResponse]):
     def __init__(self, session: AsyncSession):
         repository = AsyncRepository(User, session)
         super().__init__(repository, response_schema=UserResponse)
