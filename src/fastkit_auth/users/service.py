825382f9627991ff5b75e9999a585f4629249a11
radomirbrkovic <radomir.brkovic@pmf.edu.rs>
2026-01-28 07:57:59 +0100

    UserService: validate_create

--- src/fastkit_auth/users/service.py
+++ src/fastkit_auth/users/service.py
@@ -1,5 +1,6 @@
 from fastkit_core.services import AsyncBaseCrudService
 from fastkit_core.database import AsyncRepository
+from fastkit_core.i18n import _
 from sqlalchemy.ext.asyncio import AsyncSession
 from fastkit_auth.users.models import User
 from fastkit_auth.users.schemas import UserUpdate, UserCreate, UserResponse
@@ -8,3 +9,7 @@ class UserService(AsyncBaseCrudService[UserCreate, UserUpdate, UserResponse]):
     def __init__(self, session: AsyncSession):
         repository = AsyncRepository(User, session)
         super().__init__(repository, response_schema=UserResponse)
+
+    async def validate_create(self, data: UserCreate) -> None:
+        if await self.exists(email=data.email):
+            raise ValueError(_('validation.users.email_already_exists'))
