2cd9585ace710d23b892d2611a5932563501b667
radomirbrkovic <radomir.brkovic@pmf.edu.rs>
2026-01-29 19:48:01 +0100

    User tokes - service

---
+++ src/fastkit_auth/tokens/service.py
@@ -0,0 +1,76 @@
+from typing import Any
+from pydantic import ValidationError
+from pydantic_core import InitErrorDetails
+from fastkit_core.services import AsyncBaseCrudService
+from fastkit_core.database import AsyncRepository
+from fastkit_core.i18n import _
+from sqlalchemy.ext.asyncio import AsyncSession
+from fastkit_auth.tokens.models import UserToken
+from fastkit_auth.tokens.schemas import TokenCreate, TokenResponse
+from fastkit_auth.tokens.enums import TokenType
+from datetime import datetime, timedelta
+from uuid import UUID
+
+
+class TokenService(AsyncBaseCrudService[UserToken, TokenCreate, dict, TokenResponse]):
+    def __init__(self, session: AsyncSession):
+        repository = AsyncRepository(UserToken, session)
+        super().__init__(repository, response_schema=TokenResponse)
+
+    async def create_token(
+            self,
+            user_id: UUID,
+            token_type: TokenType,
+            expires_in_hours: int = 24
+    ) -> UserToken:
+        await self.invalidate_user_tokens(user_id, token_type)
+
+        token_data = TokenCreate(
+            user_id=user_id,
+            token=UserToken.generate_token(),
+            type=token_type,
+            expires_at=datetime.utcnow() + timedelta(hours=expires_in_hours)
+        )
+        return await self.create(token_data)
+
+    async def invalidate_user_tokens(self, user_id: UUID, token_type: TokenType) -> None:
+         await self.repository.delete_many(
+            {
+                "user_id": user_id,
+                "type":token_type,
+            }
+        )
+
+
+    async def verify_token(self, token_string: str, token_type: TokenType) -> UserToken:
+        token = await self.repository.first(token=token_string, type=token_type)
+
+        if not token:
+            raise_validation_error('token', _('tokens.invalid'))
+
+        if not token.is_valid():
+            if token.used_at:
+                raise_validation_error('token', _('tokens.already_used'))
+            else:
+                raise_validation_error('token', _('tokens.expired'))
+
+        return token
+
+    async def consume_token(self, token_string: str, token_type: TokenType) -> bool:
+        token = await self.verify_token(token_string, token_type)
+        await self.repository.delete(token.id)
+
+        return  token is not None
+
+def raise_validation_error(field: str, message: str, value: Any = None) -> None:
+    raise ValidationError.from_exception_data(
+        'ValidationError',
+        [
+            InitErrorDetails(
+                type='value_error',
+                loc=(field,),
+                input=value,
+                ctx={'error': ValueError(message)}
+            )
+        ]
+    )
