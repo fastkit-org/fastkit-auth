3a1f453dba334b372368977e5ae65ab472322135
radomirbrkovic <radomir.brkovic@pmf.edu.rs>
2026-01-31 10:58:10 +0100

    Move logic for email confirmation in user service

--- src/fastkit_auth/users/service.py
+++ src/fastkit_auth/users/service.py
@@ -7,12 +7,15 @@ from fastkit_auth.users.models import User
 from fastkit_auth.users.schemas import UserUpdate, UserCreate, UserResponse
 from fastapi_users.password import PasswordHelper
 from pydantic_core import InitErrorDetails
-from uuid import UUID
 from datetime import datetime
+from fastkit_auth.tokens.enums import TokenType
+from fastkit_auth.tokens.service import TokenService
 
 class UserService(AsyncBaseCrudService[User, UserCreate, UserUpdate, UserResponse]):
     def __init__(self, session: AsyncSession):
         repository = AsyncRepository(User, session)
+        self.token_service = TokenService(session)
+        self.session = session
         super().__init__(repository, response_schema=UserResponse)
 
     async def validate_create(self, data: UserCreate) -> None:
@@ -35,8 +38,19 @@ class UserService(AsyncBaseCrudService[User, UserCreate, UserUpdate, UserRespons
         del data['password']
         return data
 
-    async def email_confirmation(self, id: UUID) -> None:
-        await self.repository.update(id=id, data={
+    async def after_create(self, instance: User) -> None:
+        await self.token_service.create_token(
+            user_id=instance.id,
+            token_type=TokenType.EMAIL_VERIFICATION
+        )
+
+    async def email_confirmation(self, token_string: str) -> None:
+        token = await self.token_service.verify_token(
+            token_string=token_string,
+            token_type=TokenType.EMAIL_VERIFICATION
+        )
+        await self.repository.update(id=token.id, data={
             'email_verified_at': datetime.now(),
             'is_active': True
         }, commit=True)
+        await self.token_service.delete(id=token.id)
