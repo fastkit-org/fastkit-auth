25efe4f4baaea642f3f22f938de8ecb0e3566e90
radomirbrkovic <radomir.brkovic@pmf.edu.rs>
2026-02-09 20:09:16 +0100

    Update user model

--- src/fastkit_auth/users/models.py
+++ src/fastkit_auth/users/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import String, DateTime
+from sqlalchemy import String, DateTime, Boolean
 from sqlalchemy.orm import Mapped, mapped_column
 from fastkit_core.database import BaseWithTimestamps, UUIDMixin
 from typing import Optional
@@ -11,6 +11,11 @@ class User(BaseWithTimestamps, UUIDMixin):
 
     first_name: Mapped[str | None] = mapped_column(String(255))
     last_name: Mapped[str | None] = mapped_column(String(255))
+    email: Mapped[str] = mapped_column(String(320), unique=True, index=True, nullable=False)
+    hashed_password: Mapped[str] = mapped_column(String(1024), nullable=False)
+    is_active: Mapped[bool] = mapped_column(Boolean, default=False, nullable=False)
+    is_superuser: Mapped[bool] = mapped_column(Boolean, default=False, nullable=False)
+    is_verified: Mapped[bool] = mapped_column(Boolean, default=False, nullable=False)
     email_verified_at: Mapped[Optional[datetime]] = mapped_column(
         DateTime(timezone=True),
         nullable=True,
