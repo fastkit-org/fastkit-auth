305fd5e5440274b4942f627f0ed755ac830bb036
radomirbrkovic <radomir.brkovic@pmf.edu.rs>
2026-02-14 12:06:42 +0100

    Update password schema

--- src/fastkit_auth/authentication/schemas.py
+++ src/fastkit_auth/authentication/schemas.py
@@ -1,5 +1,6 @@
-from fastkit_core.validation import BaseSchema
-from pydantic import  EmailStr
+from fastkit_core.validation import BaseSchema, PasswordValidatorMixin
+from pydantic import  EmailStr, model_validator
+from fastkit_core.i18n import _
 
 class LoginRequest(BaseSchema):
     email: EmailStr
@@ -7,3 +8,14 @@ class LoginRequest(BaseSchema):
 
 class RessetPasswordRequest(BaseSchema):
     email: EmailStr
+
+class UpdatePassword(BaseSchema, PasswordValidatorMixin):
+    code: str
+    password: str
+    password_confirmation: str
+
+    @model_validator(mode="after")
+    def check_passwords_match(self):
+        if self.password != self.password_confirmation:
+            raise ValueError(_('validation.password.password_is_not_confirmed'))
+        return self
