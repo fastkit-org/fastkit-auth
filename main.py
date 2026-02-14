754034140914ad64c3aaefff4bc62a5f232c2c7e
radomirbrkovic <radomir.brkovic@pmf.edu.rs>
2026-02-14 11:24:31 +0100

    Handle validation errors by fastkit core

--- main.py
+++ main.py
@@ -1,30 +1,15 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI
 from fastkit_auth.authentication.router import router as authentication_router
 from fastkit_auth.users.router import registration_router, profile_router
 from fastkit_core.database import init_async_database
 from fastkit_core.config import ConfigManager
-from fastkit_core.validation import BaseSchema
-from fastapi.exceptions import RequestValidationError
-from fastkit_core.http import error_response
-from fastkit_core.i18n import _
-from pydantic import ValidationError
+from fastkit_core.http.exception_handlers import register_exception_handlers
 
 configuration = ConfigManager(modules=['app', 'database', 'auth'])
 init_async_database(configuration)
 
 app = FastAPI()
-@app.exception_handler(RequestValidationError)
-async def validation_exception_handler(request: Request, exc: RequestValidationError):
-    validation_error = ValidationError.from_exception_data(
-        title=exc.__class__.__name__,
-        line_errors=exc.errors()
-    )
-
-    return error_response(
-        message=_('validation.failed'),
-        errors= BaseSchema.format_errors(validation_error),
-        status_code=422
-    )
+register_exception_handlers(app=app)
 
 app.include_router(authentication_router)
 app.include_router(registration_router)
